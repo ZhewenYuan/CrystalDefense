bb0e26c2ec0a0a160a61579614b648e2c7e8e877
First Bytes <horns555@cs.utexas.edu>
2024-07-17 16:17:58 -0500

    enemy spawner stuff

---
+++ EnemySpawner.js
@@ -0,0 +1,11 @@
+class EnemySpawner extends Phaser.Time.TimerEvent
+{
+    
+    constructor(time,fn,maxEnemies)
+    {
+        super(time,fn,maxEnemies)
+    }
+    
+    // add timer
+    // lis
+} 
